165b54b27483bbbcff4c7aab9ccbd937e4d37b4b
SatoKei <Ka0sTie15@gmail.com>
2015-06-06 16:52:03 +0900

    add Xcode project

---
+++ GoodNight/GoodNight/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoodNight
+//
+//  Created by SatoKei on 2015/06/06.
+//  Copyright (c) 2015年 KeiSato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GoodNight/GoodNight/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GoodNight
+//
+//  Created by SatoKei on 2015/06/06.
+//  Copyright (c) 2015年 KeiSato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
